253fd0a5dbf1f8e371cfbd8112da475cb8ac2eef
Jeshua Hicks <jhicks7884@outlook.com>
2019-12-09 08:21:34 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -17,5 +17,4 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-"SELECT AVG(gpa) AS average_gpa_for_9th_grade FROM students;"
-end
+"SELECT AVG (students.gpa) FROM students WHERE grade = 9;"
